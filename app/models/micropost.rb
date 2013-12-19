aedd698b621945e32486e1286d37bdad7b48c671
Mihail Paleologu <mihailpaleologu@yahoo.com>
2013-12-19 22:42:17 +0200

    Initial commit

---
+++ app/models/micropost.rb
@@ -0,0 +1,20 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :title
+  attr_accessible :description
+  attr_accessible :price
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 140 }
+  validates :price, presence: true, length: {maximum: 5}
+
+  default_scope order: 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
+end
---
+++ db/migrate/20120519194108_create_microposts.rb
@@ -0,0 +1,13 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.string :title
+      t.string :description
+      t.integer :price
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :microposts, [:user_id, :created_at]
+  end
+end
